8d884943019fb413f7af3b24e4cae660068d3cce
kaiquef30 <kaiquefernandosantana@gmail.com>
2023-12-02 12:46:43 -0300

    tests added

---
+++ src/test/java/com/f30/emailservice/infra/ses/SesEmailSenderTest.java
@@ -0,0 +1,59 @@
+package com.f30.emailservice.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.f30.emailservice.core.exceptions.EmailServiceException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SesEmailSenderTest {
+
+    @Mock
+    private AmazonSimpleEmailService mockEmailService;
+
+    private SesEmailSender sesEmailSender;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        sesEmailSender = new SesEmailSender(mockEmailService);
+    }
+
+
+    @Test
+    public void sendEmail_Successful() {
+
+        var expectedRequest = new SendEmailRequest()
+                .withSource("youremail@example.com")
+                .withDestination(new Destination().withToAddresses("recipient@example.com"))
+                .withMessage(new Message()
+                        .withSubject(new Content("Test Subject"))
+                        .withBody(new Body().withText(new Content("Test Body")))
+                );
+
+
+        sesEmailSender.sendEmail("recipient@example.com", "Test Subject", "Test Body");
+
+
+        verify(mockEmailService).sendEmail(expectedRequest);
+    }
+
+    @Test(expected = EmailServiceException.class)
+    public void sendEmail_AmazonServiceException_ThrowsEmailServiceException() {
+
+
+        when(mockEmailService.sendEmail(any(SendEmailRequest.class)))
+                .thenThrow(new AmazonServiceException("Test AmazonServiceException"));
+
+
+        sesEmailSender.sendEmail("recipient@example.com", "Test Subject", "Test Body");
+    }
+
+}
